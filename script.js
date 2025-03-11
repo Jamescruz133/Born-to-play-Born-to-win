726bae6c0db88547137da4b34b27fd9b580fd258
Jamescruz133 <jmescruz143@gmail.com>
2025-03-11 21:06:03 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showContent(sport) {
-    // Itinatago ang lahat ng sections
-    document.getElementById("basketball").classList.add("hidden");
-    document.getElementById("badminton").classList.add("hidden");
-    document.getElementById("chess").classList.add("hidden");
-
-    // Pinapakita lang ang napiling section
-    document.getElementById(sport).classList.remove("hidden");
-}
